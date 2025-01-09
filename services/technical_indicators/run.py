4e4a79748d332735617ade123d4e1a847f058899
Dannyyqt <dannyyqthong@gmail.com>
2025-01-09 18:08:25 +0800

    This commit includes minor updates in technical indicator services

--- services/technical_indicators/run.py
+++ services/technical_indicators/run.py
@@ -1,9 +1,63 @@
-import talib
+from loguru import logger
+from quixstreams import Application
 
 
-def main():
-    print(talib.get_functions())
+def main(
+    kafka_broker_address: str,
+    kafka_input_topic: str,
+    kafka_output_topic: str,
+    kafka_consumer_group: str,
+    num_candles_in_state: str,
+):
+    """
+    3 Steps:
+    1. Ingest candles from kafka input topic
+    2. Computes the technical indicators
+    3. Sends the technical indicators to kafka output topic
+
+    Args:
+        kafka_broker_address (str): Kafka broker address
+        kafka_input_topic (str): Kafka input topic
+        kafka_output_topic (str): Kafka output topic
+        kafka_consumer_group (str): Kafka consumer group
+    Returns:
+        None
+    """
+    logger.info("Starting technical indicators service")
+
+    # Initialize the quix stream application
+    app = Application(
+        broker_address=kafka_broker_address,
+        consumer_group=kafka_consumer_group,
+    )
+
+    # Define input and output topics of our streaming application
+    input_topic = app.topic(
+        name=kafka_input_topic,
+        value_deserializer="json",
+    )
+
+    output_topic = app.topic(
+        name=kafka_output_topic,
+        value_deserializer="json",
+    )
+
+    sdf = app.dataframe(topic=input_topic)
+
+    sdf = sdf.update(lambda value: logger.info(f"Candle: {value}"))
+
+    sdf = sdf.to_topic(topic=output_topic)
+
+    app.run()
 
 
 if __name__ == "__main__":
-    main()
+    from config import config
+
+    main(
+        kafka_broker_address=config.kafka_broker_address,
+        kafka_input_topic=config.kafka_input_topic,
+        kafka_output_topic=config.kafka_output_topic,
+        kafka_consumer_group=config.kafka_consumer_group,
+        num_candles_in_state=config.num_candles_in_state,
+    )
